efbcec1ef2b63ab5744a1bf56667907c70a12a32
EclpticRanger <liamthomas3887@gmail.com>
2025-03-20 17:29:42 +1300

    jhvj

--- QR Code Cheacker.py
+++ QR Code Cheacker.py
@@ -1,6 +1,7 @@
 code = str(input())
 string1 = 0
 string2 = 0
+end = int(code[-1])
 
 for i in range(len(code)-1):
     if (i % 2) == 0:
@@ -8,11 +9,15 @@ for i in range(len(code)-1):
     else:
         string2 += int(code[i])
 
+string1 = str(string1)
 string2 = str(string2)
-string2 = int(code[0]) * 3
-
-
-end = int(code[-1])
-print(string1)
-print(string2)
-print(end)
+string2 = int(string2[0]) * 3
+string2 = str(string2)
+tmp = int(string2[0]) * int(string1[0])
+tmp = str(tmp)
+tmp = int(tmp[0]) + end
+print(tmp)
+if (tmp % 10) == 0:
+    print("Valid")
+else:
+    print("Not Valid")
--- ceser sifer code.py
+++ ceser sifer code.py
@@ -3,7 +3,7 @@ sifer = int
 
 def decode(text, sifer):
     output = ""
-    sifer *= -1
+    sifer = ord(text[-1])
     for i in range(len(text)):
         tmp = ord(text[i])
         tmp += sifer
@@ -13,26 +13,22 @@ def decode(text, sifer):
 
 def encode(text):
     output = ""
+    sifer = input()
+    sifer = int(sifer)
+    if sifer < 0:
+        sifer *= -1
     for i in range(len(text)):
         tmp = ord(text[i])
         tmp += sifer
         tmp = chr(tmp)
         output += tmp
+    output += chr(sifer)
     print(output)
-
-def fix_sifer_numer(shift):
-    while shift > 26 or shift < -26:
-        if shift > 26:
-            shift -= 26
-        else:
-            shift += 26
 
 while True:
     print ("-------------")
     text = str(input())
     ed = input()
-    sifer = int(input())
-    fix_sifer_numer(sifer)
     if ed == "e":
         encode(text)
     elif ed == "d":
