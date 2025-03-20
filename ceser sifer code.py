712d5a37badca825ee0bf4be3bb3506c89822eee
EclpticRanger <liamthomas3887@gmail.com>
2025-03-20 17:46:27 +1300

    rkleg

--- ceser sifer code.py
+++ ceser sifer code.py
@@ -3,7 +3,6 @@ sifer = int
 
 def decode(text, sifer):
     output = ""
-    sifer = ord(text[-1])
     for i in range(len(text)):
         tmp = ord(text[i])
         tmp += sifer
@@ -13,10 +12,6 @@ def decode(text, sifer):
 
 def encode(text):
     output = ""
-    sifer = input()
-    sifer = int(sifer)
-    if sifer < 0:
-        sifer *= -1
     for i in range(len(text)):
         tmp = ord(text[i])
         tmp += sifer
@@ -29,6 +24,10 @@ while True:
     print ("-------------")
     text = str(input())
     ed = input()
+    sifer = input()
+    sifer = int(sifer)
+    if sifer < 0:
+        sifer *= -1
     if ed == "e":
         encode(text)
     elif ed == "d":
